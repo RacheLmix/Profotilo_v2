06a04d5150f74031692ed766e535306e077a4d43
Rachel <tungxeko912@gmail.com>
2025-10-25 14:09:42 +0700

    update

--- src/complement/MainProjects.jsx
+++ src/complement/MainProjects.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 import CardProjects from '../hook/CardProjects'
 const MainProjects = () => {
   return (
-    <div>
-        <CardProjects />
+    <div className="flex flex-wrap justify-center gap-5 p-0">
+      <CardProjects
+        image="https://i.pinimg.com/736x/37/88/fc/3788fc86e7c8110ef00cfab3bde27203.jpg"
+        title="Project Daily UI"
+        description="website design for daily UI challenges.I designed over 100+ UI screens for various applications, focusing on user experience and visual aesthetics."
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/b2/8e/c9/b28ec9727db11430bdc9cee19e7b802b.jpg"
+        title="Project AI Vtuber Komoto"
+        description="An AI-powered virtual YouTuber (VTuber) that uses advanced machine learning algorithms to create engaging and interactive content for viewers."
+      />
+      <CardProjects
+        image="https://i.pinimg.com/474x/55/63/d0/5563d02e6085d6a8aac717424baba1db.jpg"
+        title="Project Todo List App"
+        description="A sleek and intuitive to-do list application designed to help users organize their tasks efficiently with a user-friendly interface."
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/33/72/48/337248c0d1d11339966b77472070a3bb.jpg"
+        title="Project DragonBall Online Game"
+        description="An immersive online multiplayer game based on the DragonBall universe, featuring real-time combat, character customization, and epic quests."
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/99/41/7d/99417d04b60bf2c238dcd4a88207f540.jpg"
+        title="Project Fumetsu Chat"
+        description="A revolutionary chat application that leverages AI to provide intelligent responses, seamless user experience, and enhanced communication features and security."
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/37/88/fc/3788fc86e7c8110ef00cfab3bde27203.jpg"
+        title="Comming Soon Projects"
+        description="Well coming soon Projects in the future 2026 (the new projects 2026)"
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/37/88/fc/3788fc86e7c8110ef00cfab3bde27203.jpg"
+        title="Comming Soon Projects"
+        description="Well coming soon Projects in the future 2026 (the new projects 2026)"
+      />
+      <CardProjects
+        image="https://i.pinimg.com/736x/37/88/fc/3788fc86e7c8110ef00cfab3bde27203.jpg"
+        title="Comming Soon Projects"
+        description="Well coming soon Projects in the future 2026 (the new projects 2026)"
+      />
     </div>
   )
 }
--- src/hook/CardProjects.jsx
+++ src/hook/CardProjects.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
-const CardProjects = () => {
+import '../styles/CardProjects.css'
+import TargetCursor from './TargetCursor'
+const CardProjects = ({image, title, description}) => {
   return (
-    <div>CardProjects</div>
+    <>
+      <TargetCursor
+        spinDuration={2}
+        hideDefaultCursor={true}
+      />
+      <div className="card-container cursor-target">
+        <div className="card">
+          <img
+            src={image}
+            alt="Project"
+          />
+          <div className="card-content">
+            <h3>{title}</h3>
+            <p>
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
