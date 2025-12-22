cb32a44db7ee0059e7a37f8a4ddac4852b1191f2
Rachel <tungxeko912@gmail.com>
2025-12-22 19:01:47 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode, Suspense } from 'react'
+import React, { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import NotFound from './page/NotFound.jsx'
 import Snaptik from './page/Snaptik.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Loading from './page/Loading.jsx'
+const App = React.lazy(() => import('./App.jsx'))
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "*", element: <NotFound /> },
